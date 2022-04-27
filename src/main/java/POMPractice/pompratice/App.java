74711c5386af7aaea27519b5e5882d5dfd954144
Pravin Junghare <pravinjunghare01@gmail.com>
2022-04-27 14:16:00 +0530

    first day

---
+++ src/main/java/POMPractice/pompratice/App.java
@@ -0,0 +1,13 @@
+package POMPractice.pompratice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
